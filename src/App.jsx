512b1319d4f048faaacabd4539d3f671c3c20d39
MS-Deepika-7 <deepikadeepu782006@gmail.com>
2026-08-03 16:40:46 +0530

    tashpush

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Skills from './components/Skills/Skills'
 import Projects from './components/Projects/Projects'
 import Contact from './components/Contact/Contact'
 
-
 function App() {
   const [count, setCount] = useState(0)
  
